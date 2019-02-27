3a2e4614a2ede78208ff50afa91697a48bffd91b
Carlos Bruno <cbcarlos08@gmail.com>
2019-02-27 12:42:29 -0400

    Codigo fonte

--- backend.php
+++ backend.php
@@ -8,6 +8,14 @@ $objeto1 = array(
     's_read' => 1,
     'time' => '2019-02-26 10:32:55'
 );
+$objeto4 = array(
+    'id' => 1,
+    'from' => 1,
+    'to' => 2,
+    'message' => 'Mensagem 2',
+    's_read' => 1,
+    'time' => '2019-02-28 10:32:55'
+);
 
 $objeto2 = array(
     'id' => 2,
@@ -26,9 +34,28 @@ $objeto3 = array(
     's_read' => 1,
     'time' => '2019-02-27 08:38:20'
 );
+$objeto5 = array(
+    'id' => 3,
+    'from' => 1,
+    'to' => 2,
+    'message' => 'Mensagem 3',
+    's_read' => 1,
+    'time' => '2019-02-28 08:38:20'
+);
+$objeto6 = array(
+    'id' => 3,
+    'from' => 1,
+    'to' => 2,
+    'message' => 'Mensagem 3',
+    's_read' => 1,
+    'time' => '2019-03-03 08:38:20'
+);
 $messages[] = (object) $objeto1;
 $messages[] = (object) $objeto2;
 $messages[] = (object) $objeto3;
+$messages[] = (object) $objeto4;
+$messages[] = (object) $objeto5;
+$messages[] = (object) $objeto6;
 
 //print_r($messages);
 
@@ -46,7 +73,6 @@ foreach ($messages as $message){
         'type'      => $message->from == 1 ? 'sender' : 'receiver',
         'name'      => $message->from == 2 ? 'You' : 0
     ];
-
     $timestamp = strtotime($message->time);
     $date = date('d-m-Y', $timestamp);
     if ( !in_array_r( $date, $sorted_date  ))
@@ -58,8 +84,9 @@ foreach ($messages as $message){
     }
     else
     {
-        $key = array_search($date, $sorted_date);
-        
+        //$key = array_search($date, $sorted_date['data']);
+        $key = array_seach_f($date, $sorted_date )  ;  
+        echo "Key $key \n";
         $sorted_date[$key]['message'][] = $chat;
     }
 }
@@ -67,16 +94,28 @@ $response = [
     'success' => true,
     'thread'  => $sorted_date
 ];
-//print_r( $sorted_date );
+print_r( $sorted_date );
 header('Content-Type: application/json');
-echo json_encode($response);
-function in_array_r($needle, $haystack, $strict = false) {
-    foreach ($haystack as $item) {
-        if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
-            return true;
-        }
-    }
-
-    return false;
+//echo json_encode($response);
+function in_array_r($item , $array) {
+    return preg_match('/"'.preg_quote($item, '/').'"/i' , json_encode($array));
 }
+
+ function array_seach_f( $data, $array )
+ {
+     $index = -1;
+     foreach ($array as $key => $value) {
+         
+         if( in_array( $data, $value ) ){
+                
+                echo $data."\n";
+                $index = $key;
+                break;
+            } 
+       
+        
+     }
+     
+     return $index;
+ }
 ?>
